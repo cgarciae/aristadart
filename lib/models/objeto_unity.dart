43c7b8247a086266290fcfc2a05ea62f49ff4757
cgarciae <cgarcia.e88@gmail.com>
2015-02-02 10:24:02 -0500

    objeto unity

--- lib/models/objeto_unity.dart
+++ lib/models/objeto_unity.dart
@@ -2,10 +2,26 @@ part of arista;
 
 class ObjetoUnity
 {
+    @Id() String id;
+    @ReferenceId() String userFile;
+    @Field() String name;
+}
+
+class ObjetoUnitySend extends ObjetoUnity
+{
+    @Field() bool active;
+    @Field() int version;
+    @ReferenceId() String screenshotId;
+}
+
+class ObjetoUnityAdmin extends ObjetoUnitySend
+{
     @Field () String path;
-    @Field() String get url_objeto => path != null && path != '' ? localHost + path : '';
-    @Field () int version;
-    
-    ObjetoUnity ();
-    
+    @Field() String get url_objeto
+    {
+        if (path == null || path == '' || ! active)
+            return "";
+        
+        return localHost + path;
+    }
 }
