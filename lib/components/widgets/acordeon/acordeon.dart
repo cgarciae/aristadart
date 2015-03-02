69095d064720590b219884adba9321a861a6ff86
Esteban Higuita García <estebanhiguita@gmail.com>
2015-03-02 18:08:44 -0500

    acordion: inicio

---
+++ lib/components/widgets/acordeon/acordeon.dart
@@ -0,0 +1,16 @@
+part of aristadart.client;
+
+@Component
+(
+    selector : "acordeon",
+    templateUrl: 'components/widgets/acordeon/acordeon.html',
+    useShadowDom: false
+)
+class Acordeon
+{
+    bool mostrarContenido =false;
+    
+    Acordeon(){
+        
+    }
+}
